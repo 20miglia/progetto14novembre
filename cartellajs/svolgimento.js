ffb487753a4f3ab4f630d2a073c46d99eef33ad1
Gianluca Ventimiglia <20migliagianluca@gmail.com>
2024-11-15 20:57:15 +0100

    first commit

---
+++ cartellajs/svolgimento.js
@@ -0,0 +1,41 @@
+//Risposta numero 1//
+
+let risposta1 = "I principali datatype in JavaScript sono: numeri, string, boolean, oggetti, array, null e undefined. I Numeri possono essere interi e decimali, ed esistono valori speciali come +infinity e -infinity e Nan. Le stringhe sono sequenze di caratteri delimitate da apici singoli o doppi. I booleani sono un'entità logica e possono avere valore vero o falso. Gli oggetti sono raccolte di coppie chiave-valore. Gli array sono liste di dati contenenti diversi valori. Null rappresenta un valore vuoto, la variabile viene privata del suo contenuto. Undefined quando a una variabile non è stato assegnato alcun valore"
+console.log(risposta1)
+
+//Risposta numero 2//
+
+let risposta2 = "Gli oggetti sono binomi dati da una categoria e il suo relativo valore"
+console.log(risposta2)
+
+//Risposta numero 3//
+
+let addizione = 12 + 20
+console.log(addizione)
+
+//Risposta numero 4//
+
+let x = 12
+
+//Risposta numero 5//
+
+let name = "Gianluca"
+
+//Risposta numero 6//
+
+let y = 4
+let risultato = x - y
+console.log(risultato)
+
+//Risposta numero 7//
+
+let name1 = "john"
+let name2 = "John"
+let confronto = name1 !== name2
+console.log(confronto)
+
+
+name1 = name1.toLowerCase()
+name2 = name2.toLowerCase()
+let paragone = name1 === name2
+console.log(paragone)
